78763619fa410efde1cfe3d5404ce82cd5c4a14e
antonsos <anton1986.2012@gmail.com>
2019-07-19 22:49:27 +0700

    added two routes users and tours

---
+++ routes/tours.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+router
+.route('/')
+.get((req, res) => {
+  res.json({ message: 'route not work'})
+})
+.post((req, res) => {
+  res.json({ message: 'route not work'})
+});
+
+router
+.route('/:id')
+.get((req, res) => {
+  res.json({ message: 'route not work'})
+})
+.path((req, res) => {
+  res.json({ message: 'route not work'})
+})
+.delete((req, res) => {
+  res.json({ message: 'route not work'})
+});
+
+module.exports = router;
